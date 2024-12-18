6cffb3879fac96a91a196ee846e955e7bfd434da
Sha Doh <ncboyboyboy@gmail.com>
2024-12-19 00:41:40 +0700

    Click logo will go to profile section, Add nav bar background color transparent when scroll

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ document.getElementById('year').innerHTML = new Date().getFullYear();
 let prevScrollpos = window.pageYOffset;
 
 window.onscroll = function () {
+  const desktopNav = document.getElementById("desktop-nav");
+  const hamburgerNav = document.getElementById("hamburger-nav");
+
+    // Show or hide navbar
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("desktop-nav").style.top = "0";
@@ -22,5 +26,15 @@ window.onscroll = function () {
     document.getElementById("desktop-nav").style.top = "-110px";
     document.getElementById("hamburger-nav").style.top = "-110px";
   }
+
+  // Change background color when scrolling
+  if (currentScrollPos > 180) {
+    desktopNav.style.backgroundColor = "white"; // Darker background when scrolling
+    hamburgerNav.style.backgroundColor = "white"; // Darker background when scrolling
+  } else {
+    desktopNav.style.backgroundColor = "transparent"; // Original background at the top
+    hamburgerNav.style.backgroundColor = "transparent"; // Original background at the top
+  }
+
   prevScrollpos = currentScrollPos;
 };
