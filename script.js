ce0699cf7c62b410165bbcde4f105122b46668c0
Sha Doh <ncboyboyboy@gmail.com>
2024-12-18 23:14:57 +0700

    add nav bar disappear scroll

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function toggleMenu() {
 // Testing Copyright Date auto update
 // let date = new Date();
 document.getElementById('year').innerHTML = new Date().getFullYear();
+
+// nav bar scroll disappear
+// Hide or show the navbar on scroll
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("desktop-nav").style.top = "0";
+    document.getElementById("hamburger-nav").style.top = "0";
+  } else {
+    document.getElementById("desktop-nav").style.top = "-110px";
+    document.getElementById("hamburger-nav").style.top = "-110px";
+  }
+  prevScrollpos = currentScrollPos;
+};
